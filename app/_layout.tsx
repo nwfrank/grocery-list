cdc8a9b7922bd418ae5f31e75e32839e09f2945c
nwfrank <natefrank2048@gmail.com>
2025-03-30 20:51:51 -0500

    feat: create expo project

---
+++ app/_layout.tsx
@@ -0,0 +1,32 @@
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+import {Text} from 'react-native'
+
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <SafeAreaView>
+      <Text>Testing</Text>
+    </SafeAreaView>
+  );
+}
