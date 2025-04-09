c56a994717e830d7d24a2a548750365e01830bd4
nwfrank <natefrank2048@gmail.com>
2025-04-08 19:45:29 -0500

    formatted files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,33 @@
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
-import 'react-native-reanimated';
-import {StyleSheet, ScrollView, View, Text, FlatList} from 'react-native'
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+import "react-native-reanimated";
+import { StyleSheet, ScrollView, View, Text, FlatList } from "react-native";
 
-import { SafeAreaView } from 'react-native-safe-area-context';
-import ListItem from '@/components/ListItem';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import { SafeAreaView } from "react-native-safe-area-context";
+import ListItem from "@/components/ListItem";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
-  const [items, setItems] = useState([{id: 1, text: 'test1'}, {id: 2, text: 'test2'}, {id: 3, text: 'test3'}, {id: 4, text: 'test4'}, {id: 5, text: 'test5'}])
+  const [items, setItems] = useState([
+    { id: 1, text: "test1" },
+    { id: 2, text: "test2" },
+    { id: 3, text: "test3" },
+    { id: 4, text: "test4" },
+    { id: 5, text: "test5" },
+  ]);
 
   const handleDeleteItem = (id: number) => {
-    const updatedData = items.filter((item) => item.id !== id)
+    const updatedData = items.filter((item) => item.id !== id);
     setItems(updatedData);
-  }
+  };
 
   useEffect(() => {
     if (loaded) {
@@ -33,13 +39,19 @@ export default function RootLayout() {
     return null;
   }
 
-  const renderItem = ({item}: {item: {id: number, text: string}}) => <ListItem item={item} onDelete={handleDeleteItem} />;
+  const renderItem = ({ item }: { item: { id: number; text: string } }) => (
+    <ListItem item={item} onDelete={handleDeleteItem} />
+  );
 
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList data={items} renderItem={renderItem} keyExtractor={(item) => item.id}/>
+      <FlatList
+        data={items}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
       <View style={styles.add}>
-         <Icon name="add" size={40} color="white" style={styles.addIcon} />
+        <Icon name="add" size={40} color="white" style={styles.addIcon} />
       </View>
     </SafeAreaView>
   );
@@ -48,28 +60,26 @@ export default function RootLayout() {
 const styles = StyleSheet.create({
   container: {
     padding: 10,
-    position: 'relative',
-    flex: 1
+    position: "relative",
+    flex: 1,
   },
   scrollview: {
-    flexDirection: 'column',
+    flexDirection: "column",
   },
   add: {
-    backgroundColor: 'green',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "green",
+    justifyContent: "center",
+    alignItems: "center",
     height: 65,
     width: 65,
     borderRadius: 50,
-    position: 'absolute',
+    position: "absolute",
     bottom: 25,
     right: 25,
   },
-  addIcon: {
-    
-  },
+  addIcon: {},
   pad: {
     height: 60,
-    width: '100%'
-  }
+    width: "100%",
+  },
 });
